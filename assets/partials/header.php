f0c081a02323e95a473689c089e5a072531b0367
Aislan Santos Aprovar <aislan_85@yahoo.com.br>
2020-08-18 17:25:50 -0300

    Estudo Login e CSS

--- assets/partials/header.php
+++ assets/partials/header.php
@@ -1,6 +1,25 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <span class="navbar-brand"><img src="assets/image/logo.png" alt=""></span>
-    <span class="navbar-text">
-        <p>Bem vindo <b><i> <?= $_SESSION['user'] ?> </i></b> !</p>
-    </span>
+
+    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Cadastro
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Soja</a>
+                    <a class="dropdown-item" href="#">Cliente</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Relatório</a>
+                </div>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?acao='sair'">Sair <span class="sr-only">(página atual)</span></a>
+            </li>
+    </div>
+
 </nav>
+
+
+<p>Bem vindo <b><i> <?= $_SESSION['user'] ?> </i></b> !</p>
