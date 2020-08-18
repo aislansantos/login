628b926fa5221e1af269a5102fdb0d5426cb1dfb
Aislan Santos Aprovar <aislan_85@yahoo.com.br>
2020-08-18 17:08:02 -0300

    Estudo Login e CSS

---
+++ assets/partials/header.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-dark bg-dark">
+    <span class="navbar-brand"><img src="assets/image/logo.png" alt=""></span>
+    <span class="navbar-text">
+        <p>Bem vindo <b><i> <?= $_SESSION['user'] ?> </i></b> !</p>
+    </span>
+</nav>
